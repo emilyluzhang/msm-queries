9605bb4f5d9117548be327d1d05e34ebfc2ced1c
emilyluzhang <55498035+emilyluzhang@users.noreply.github.com>
2024-05-11 03:17:40 +0000

    directors with details

---
+++ app/controllers/directors_controller.rb
@@ -0,0 +1,13 @@
+class DirectorsController < ApplicationController 
+  def index 
+    render({ :template => "director_templates/list" })
+  end 
+  def show
+    @dir_num = params.fetch("dir_id")
+
+    @matching_records = Director.where({:id => @dir_num})
+    @the_director = @matching_records.at(0)
+
+    render({ :template => "director_templates/details"}) 
+  end 
+end 
---
+++ lib/tasks/test.rake
@@ -0,0 +1,1 @@
+actors = Actor.all
